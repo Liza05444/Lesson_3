3fbf8dc7087b96fbd9d60d7fb4ed0224c0378cc4
Liza <73343991+Liza05444@users.noreply.github.com>
2023-12-03 20:33:08 +0300

    Add files via upload

---
+++ Dictionary/Menu/menu.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <iostream>
+
+struct Dict {
+    char* eng;
+    char* rus;
+    char* first_synonym;
+    char* second_synonym;
+};
+
+namespace HashTable {
+    struct Table {
+        Dict** words;
+        int size;
+    };
+
+    unsigned long long create_hash(char* word);
+    void print_synonyms(Table* table, char* word);
+    void print_rus_translation(Table* table, char* word);
+    void print_eng_translation(Table* table, char* word);
+}
